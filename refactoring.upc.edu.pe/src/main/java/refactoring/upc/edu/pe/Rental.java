73d22057d06a7d12c3f75b609cf3c6631990c0f5
tchambil <antony_epis@hotmail.com>
2019-11-03 22:40:54 -0500

    first commit

---
+++ refactoring.upc.edu.pe/src/main/java/refactoring/upc/edu/pe/Rental.java
@@ -0,0 +1,20 @@
+package refactoring.upc.edu.pe;
+
+public class Rental
+{
+	public Rental (Movie movie, int daysRented) {
+		this.movie 		= movie;
+		this.daysRented = daysRented;
+	}
+	
+	public int getDaysRented () {
+		return daysRented;
+	}
+	
+	public Movie getMovie () {
+		return movie;
+	}
+	
+	private Movie movie;
+	private int daysRented;
+}
